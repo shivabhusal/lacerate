1e1082e7e749e00e7bbeb7b070699b7fa6cb4911
Shiva / John <hotline.shiva@gmail.com>
2017-03-12 11:51:32 +0545

    Displaying number of reports and queryed keywords in last 30 days - Analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,18 @@ module ApplicationHelper
         done:        'label label-success'
     }[status.to_sym] || 'label label-primary'
   end
+
+  def report_analytics_data(reports)
+      data = (0..(Integer(params[:chart_filter]) || 30)).to_a.map do |i|
+        <<-EOS
+         {
+             y: '#{i.days.ago.strftime('%Y-%m-%d')}',
+             a: #{reports.from_date(i.days.ago).count},
+             b: #{reports.from_date(i.days.ago).map(&:keyword_count).sum}
+         }
+        EOS
+      end
+
+    "[#{data.join(',')}]".html_safe
+  end
 end
