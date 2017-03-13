e55fd40dfd182c6659ec8d8014c2f05c0d5737bf
Shiva / John <hotline.shiva@gmail.com>
2017-03-13 11:10:51 +0545

    Custom queries setup using Query Model and QueriesController
    - Null object pattern introduced

---
+++ app/models/concerns/query.rb
@@ -0,0 +1,21 @@
+# How many URLs contain the word “technology” in AdWords
+# How many times a specific URL shows up in SEO.
+# How many keywords have URLs in SEO with 2+ /’s or 1+ >
+
+class Query
+  attr_reader :params, :user
+
+  def initialize(params = {})
+    @params = params
+    @user   = params[:user]
+  end
+
+  def execute
+    result         = {}
+    result[:word]  = user.search_results.word_query(params[:word])  if params[:word].present?
+    result[:url]   = user.search_results.url_query(params[:url])    if params[:url].present? # We assume URL is a fragment od actual URL
+    result[:caret] = user.search_results.caret_query                if params[:caret] == 'on'
+
+    result
+  end
+end
